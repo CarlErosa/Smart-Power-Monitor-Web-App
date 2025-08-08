f873c9acb5c36c7983b458f7cfb3be6f97ce66be
Carl Erosa <carlmelvinerosa3@gmail.com>
2025-08-08 19:42:20 +0800

    feat: restructure Home component to include How It Works and Pricing sections

--- smartpower/src/app/page.tsx
+++ smartpower/src/app/page.tsx
@@ -1,17 +1,33 @@
 import Header from '@/components/sections/Header';
 import Hero from '@/components/sections/Hero';
+import HowitWorks from '@/components/sections/HowitWorks';
+import { Zap, TrendingDown, Activity , Cog, Shield, BarChart, CheckCircle } from 'lucide-react';
+import { Metadata } from 'next';
+import Pricing from '@/components/sections/Pricing';
 
-
+export const metadata: Metadata = {
+  title: "Welcome to SmartVolt",
+  description: "",
+};
 
 export default function Home() {
   return (
   <div>
    {/*Header Section*/}
-   <Header />
+  <Header />
 
    {/*Hero Section*/}
   <Hero />
 
-  </div>
+   {/*How It Works Section*/}
+  <HowitWorks />
+
+   {/* Pricing Section */}
+   <Pricing />
+
+</div>
+
+
+
   );
 }
